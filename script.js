6eaab66e82c2614a2f22d8fcbdb3a1fc22e079e5
dukedukex2-wq <dukedukex2@gmail.com>
2025-11-01 19:36:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('#menu');
+const navbar = document.querySelector('.navbar');
+// Focuses on the icon
+const icon = menu.querySelector('i');
+
+menu.onclick = () => {
+    icon.classList.toggle('ri-menu-3-fill');
+    icon.classList.toggle('ri-close-circle-line');
+    navbar.classList.toggle('open');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('open');
+    icon.classList.remove('ri-close-circle-line');
+    icon.classList.add('ri-menu-3-fill');
+
+}
+
